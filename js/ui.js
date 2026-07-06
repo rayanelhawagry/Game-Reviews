baf1ba41afcab25cd82bd78b4eb98f363285e4d0
Rayan Elhawagry <se.mycareer@gmail.com>
2026-07-06 17:00:11 +0300

    Navbar active links

--- js/ui.js
+++ js/ui.js
@@ -1,30 +1,22 @@
 "use strict";
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('#navbar-menu');
-// const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll('.nav-link');
+const firstLink = document.querySelector('.firstLink');
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const navItems = document.querySelectorAll('.nav-link');
-    
-    navItems.forEach(item => {item.addEventListener('click', function () {
-        navItems.forEach(navItem => navItem.classList.remove('active'));
+// Navbar active links.
+firstLink.classList.add('active');
+navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+        navLinks.forEach(item => item.classList.remove('active'));
         this.classList.add('active');
     });
 });
-});
 
 
-// navLinks.forEach(link => {
-//     link.addEventListener("click", () => {
-//         // Remove 'active' class from all links
-//         navLinks.forEach(item => item.classList.remove('active'));
-//         // Add 'active' class to the clicked link
-//     });
-// });
-
+// Navbar Toggle.
 navbarToggle.addEventListener("click", () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
-    // navbarMenu.classList.toggle('d-none');
 });
